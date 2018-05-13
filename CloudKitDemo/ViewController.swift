438446d2d0966860d84d9679f6fa2844c78e6ae2
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-05-13 12:34:16 +0200

    Start of playing with CloudKit.

--- CloudKitDemo/ViewController.swift
+++ CloudKitDemo/ViewController.swift
@@ -5,14 +5,23 @@
 //  Created by Søren Knudsen on 12/05/2018.
 //  Copyright © 2018 Søren Knudsen. All rights reserved.
 //
+//This is a cloudkit demo to lear it better.
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Init of CloudKit
+        
+        let contariner = CKContainer.default()
+        let publicDatabase = contariner.publicCloudDatabase
+        let privateDatabase = contariner.privateCloudDatabase
+        let sharedDatabase = contariner.sharedCloudDatabase
+        
     }
 
     override func didReceiveMemoryWarning() {
