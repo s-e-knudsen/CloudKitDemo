5cd3843b2d75083a5e62b7a5e81ed6681056f5b8
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-05-13 12:57:52 +0200

    Added the save to CloudKit demo.

--- CloudKitDemo/ViewController.swift
+++ CloudKitDemo/ViewController.swift
@@ -19,9 +19,30 @@ class ViewController: UIViewController {
         
         let contariner = CKContainer.default()
         let publicDatabase = contariner.publicCloudDatabase
-        let privateDatabase = contariner.privateCloudDatabase
-        let sharedDatabase = contariner.sharedCloudDatabase
+        //let privateDatabase = contariner.privateCloudDatabase
+        //let sharedDatabase = contariner.sharedCloudDatabase
         
+        //Creating a record ID in CloudKit
+        let artworkRecordID = CKRecordID(recordName: "115")
+        
+        //Create a Record object
+        let artworkRecord = CKRecord(recordType: "Artwork", recordID: artworkRecordID)
+        
+        //Sets the fealds in the redord
+        artworkRecord["title"] = "MacKerricher State Perk" as NSString
+        artworkRecord["artist"] = "Mei Chen" as NSString
+        artworkRecord["address"] = "Fort Bragg, CA" as NSString
+        
+        //Save the recoed to Cloudkit
+        publicDatabase.save(artworkRecord) { (record, error) in
+            if let error = error {
+                print(error)
+                return
+                
+            } else {
+                print("record saved successuly")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
